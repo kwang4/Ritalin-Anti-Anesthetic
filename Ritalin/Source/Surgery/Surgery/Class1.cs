82ba6c22d16c24f6ac236bdf9750df8e7d4542d4
kwang4 <52985771+kwang4@users.noreply.github.com>
2020-07-12 12:49:55 -0400

    Add files via upload
    Updated compatibility for 1.1 and 1.0

---
+++ Ritalin/Source/Surgery/Surgery/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+namespace Surgery
+{
+    public class HediffCompProperties_WakeAnesthetic : CompProperties_Drug
+    {
+        public HediffCompProperties_WakeAnesthetic()
+        {
+            compClass = typeof(HediffComp_WakeAnesthetic);
+        }
+    }
+    public class HediffComp_WakeAnesthetic:CompDrug
+    {
+        public override void PostIngested(Pawn ingester)
+        {
+            if(ingester.health.hediffSet.HasHediff(HediffDefOf.Anesthetic))
+            {
+                Hediff h = ingester.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.Anesthetic);
+                if (h != null)
+                {
+                    ingester.health.RemoveHediff(h);
+                }
+            }
+            base.PostIngested(ingester);
+        }
+    }
+}
